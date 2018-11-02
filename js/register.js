245886aa1dda1ed0edb4fbfd6d3ff4c35df10c31
Asianu <l6leung@ucsd.edu>
2018-11-02 00:00:03 -0700

    fixed modal on register page

--- js/register.js
+++ js/register.js
@@ -1,9 +1,5 @@
 
-$("#cancelButton").click(function() {
+$(".retHomeBtn").click(function() {
 	window.location = $(this).find("a").attr("href");
 	return false;
 });
-
-$('#exampleModal').on('shown.bs.modal', function () {
-	$('#exampleModal').trigger('focus')
-  })
