5417e804510b2068dffbd202b7784383b458a205
Asianu <l6leung@ucsd.edu>
2018-11-08 22:40:14 -0800

    register backend functioning

--- public/js/register.js
+++ public/js/register.js
@@ -23,6 +23,45 @@ $(document).ready(function() {
 		});
 	});
 
+	// to update the database when a form has been submitted
+	$("#form-submit-button").click(function() {
+		var input = $('form').serializeArray();
+
+		// parse input data into a user JSON object
+		var user = {};
+		$.each(input, function(index, form_obj) {
+			if (form_obj.name == "expertise") {
+				switch(form_obj.value) {
+					case '1': user[form_obj.name] = "Beginner";
+						break;
+					case '2': user[form_obj.name] = "Novice";
+						break;
+					case '3': user[form_obj.name] = "Intermediate";
+						break;
+					case '4': user[form_obj.name] = "Advanced";
+						break;
+					case '5': user[form_obj.name] = "Expert";
+						break;
+				}
+			}
+			else {
+				user[form_obj.name] = form_obj.value;
+			}
+		});
+		console.log(user);
+
+		// get a new key for post
+		var newPostKey = firebase.database().ref().child('mentors').push().key;
+
+		var updates = {};
+		updates['/mentors/' + newPostKey] = user;
+
+		firebase.database().ref().update(updates);
+
+		console.log(newPostKey);
+
+	})
+
 	// make sure name field is properly generated
 	var template = Handlebars.compile($("#form-name-value-template").html());
 	$(template(JSON.parse(strg.getItem('user')))).insertAfter('#form-name-label');
