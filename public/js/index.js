f0faf194cdf2d88e2aaf186ba97451bc632efeff
Asianu <l6leung@ucsd.edu>
2018-11-09 01:08:25 -0800

    disabled be button on front page if not signed in yet

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+var strg = window.localStorage;
+
+$(document).ready(function() {
+
+	if (strg.getItem('signin_token') != 'true') {
+		$('#landing-be').addClass('btn-disabled').removeClass('btn-secondary').attr('onclick', '#');
+	}
+	else {
+		$('#landing-be').removeClass('btn-disabled').addClass('btn-secondary').attr('onclick', "window.location.href='register.html'");
+	}
+});
