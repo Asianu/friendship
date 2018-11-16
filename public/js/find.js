e005bb9877c2a4f75cf948e9566c258ed5904b55
Asianu <l6leung@ucsd.edu>
2018-11-15 19:59:45 -0800

    find page link to database, needs profile pictures

--- public/js/find.js
+++ public/js/find.js
@@ -5,15 +5,12 @@ $(document).ready(function() {
 
 	mentorRef.orderByChild('name').on('child_added', function(mentor) {
 		// pre-compile the template
-		var template = Handlebars.compile($("#mentor-entry-template").html());
-		$("#find-table-body").append(template(mentor.val()));
+		// var template = Handlebars.compile($("#mentor-entry-template").html());
+		// $("#find-table-body").append(template(mentor.val()));
+
+		var mentor_info = mentor.val();
+		var template = Handlebars.compile($("#find-card-template").html());
+		$("#mentors-row").append(template(mentor_info));
 	});
 
-});
-
-/* JQuery toggle for modal */
-jQuery(document).ready(function($) {
-    $(".clickable-row").click(function() {
-		$('#exampleModal').modal('show');
-    });
 });
