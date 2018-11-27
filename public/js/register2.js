2a9b5dd9862cf7e59c77d8a0acf4863bd9d0db65
Asianu <l6leung@ucsd.edu>
2018-11-26 23:15:34 -0800

    to merge

--- public/js/register2.js
+++ public/js/register2.js
@@ -3,53 +3,28 @@ var counter = 1;
 
 $(document).ready(function() {
 
-	// to populate the activites field
-	var activityRef = firebase.database().ref('/activities');
-	activityRef.orderByValue().on("child_added", function(activity) {
-		var template = Handlebars.compile($("#form-activity-template").html());
-		$("#form-activity-dropdown").append(template({'activity' : activity.val()}));
-	});
-
-	// for validation purposes
-	window.addEventListener('load', function() {
+	window.addEventListener('load', function(e) {
 	    // Fetch all the forms we want to apply custom Bootstrap validation styles to
 	    var forms = document.getElementsByClassName('needs-validation');
 	    // Loop over them and prevent submission
 	    var validation = Array.prototype.filter.call(forms, function(form) {
 	      form.addEventListener($("#submit"), function(event) {
+	      	console.log('here');
+	      	event.preventDefault();
+	      	event.stopPropagation();
 	        if (form.checkValidity() === false) {
+	        	console.log('here');
 	          event.preventDefault();
 	          event.stopPropagation();
 	        }
 	        else {
+	        	event.preventDefault();
 	        	console.log('there');
 	        	var input = $('form').serializeArray();
 
 				// parse input data into a user JSON object
 				var user = {};
-				$.each(input, function(index, form_obj) {
-					if (form_obj.name == "expertise") {
-						var expertiseLevel = parseInt(form_obj.value)
-						if (expertiseLevel == 1){
-							user[form_obj.name] = "No Experience"
-						} else if (expertiseLevel <= 20){
-							user[form_obj.name] = "Beginner";
-						} else if (expertiseLevel <= 40) {
-							user[form_obj.name] = "Novice";
-						} else if (expertiseLevel <= 60) {
-							user[form_obj.name] = "Intermediate";
-						} else if (expertiseLevel <= 80) {
-							user[form_obj.name] = "Advanced";
-						} else if (expertiseLevel < 100) {
-							user[form_obj.name] = "Expert";
-						} else {
-							user[form_obj.name] = "Master";
-						}
-					}
-					else {
-						user[form_obj.name] = form_obj.value;
-					}
-				});
+				
 				user['uid'] = firebase.auth().currentUser.uid;
 				console.log(user);
 
