75af01681af1cae6facc3a4dea51f481dd6e6f21
Asianu <l6leung@ucsd.edu>
2018-11-27 01:58:25 -0800

    profile page fixes

--- public/js/profile.js
+++ public/js/profile.js
@@ -13,36 +13,41 @@ $(document).ready(function() {
 
 			var mentor_template = Handlebars.compile($("#mentor-table-template").html());
 			firebase.database().ref('/mentor_list').child(mentor_list_id).once('value').then(function(mentors) {
-				$.each(mentors.val(), function(key) {
-					firebase.database().ref('/mentors').child(key).once('value').then(function(mentor) {
-						$("#mentor-table-body").append(mentor_template(mentor.val()));
+				if(mentors.val() != null) {
+
+					$.each(mentors.val(), function(key) {
+						firebase.database().ref('/mentors').child(key).once('value').then(function(mentor) {
+							$("#mentor-table-body").append(mentor_template(mentor.val()));
+						});
+						$("#empty-mentor-row").hide();
 					});
-					$("#empty-mentor-row").hide();
-				});
+				}
 			});
 
 			var mentee_template = Handlebars.compile($("#mentee-table-template").html());
 
 			// read data to get entry values for each mentee
 			firebase.database().ref('/mentee_list').child(mentee_list_id).once('value').then(function(mentees) {
-				$.each(mentees.val(), function(key, value) {
-					$.each(value, function(uid, activity) {
-						var mentee_entry = {};
+				if (mentees.val() != null) {
+					$.each(mentees.val(), function(key, value) {
+						$.each(value, function(uid, activity) {
+							var mentee_entry = {};
 
-						// get the data for the mentor's specific-activity
-						firebase.database().ref('/mentors').child(key).once('value').then(function(mentor) {
-							mentee_entry['activity'] = activity;
-							mentee_entry['specific-activity'] = mentor.val()['specific-activity'];
+							// get the data for the mentor's specific-activity
+							firebase.database().ref('/mentors').child(key).once('value').then(function(mentor) {
+								mentee_entry['activity'] = activity;
+								mentee_entry['specific-activity'] = mentor.val()['specific-activity'];
 
-							// get the data for the mentee's name
-							firebase.database().ref('/users').child(uid).once('value').then(function(mentee) {
-								mentee_entry['name'] = mentee.val().displayName;
-								$("#mentee-table-body").append(mentee_template(mentee_entry));
+								// get the data for the mentee's name
+								firebase.database().ref('/users').child(uid).once('value').then(function(mentee) {
+									mentee_entry['name'] = mentee.val().displayName;
+									$("#mentee-table-body").append(mentee_template(mentee_entry));
+								});
 							});
 						});
+						$("#empty-mentee-row").hide();
 					});
-					$("#empty-mentee-row").hide();
-				});
+				}
 			});
 		});
 	});
