c50fcb47d7cafc533eb68975efda138e8e6d541e
Asianu <l6leung@ucsd.edu>
2018-12-06 09:25:34 -0800

    renamed files

--- public/js/register.js
+++ public/js/register.js
@@ -1,68 +1,82 @@
 var strg = window.localStorage;
 
+// variable that will keep track of the number of activity rows in the page
+var activity_input = {
+	activity_id: 1,
+	counter: 1
+};
+
 $(document).ready(function() {
 
-	// to populate the activites field
-	var activityRef = firebase.database().ref('/activities');
-	activityRef.orderByValue().on("child_added", function(activity) {
-		var template = Handlebars.compile($("#form-activity-template").html());
-		$("#form-activity-dropdown").append(template({'activity' : activity.val()}));
-	});
-
-	// for validation purposes
-	window.addEventListener('load', function() {
+	window.addEventListener('load', function(e) {
 	    // Fetch all the forms we want to apply custom Bootstrap validation styles to
 	    var forms = document.getElementsByClassName('needs-validation');
 	    // Loop over them and prevent submission
 	    var validation = Array.prototype.filter.call(forms, function(form) {
-	      form.addEventListener('submit', function(event) {
+	      form.addEventListener("submit", function(event) {
 	        if (form.checkValidity() === false) {
 	          event.preventDefault();
 	          event.stopPropagation();
 	        }
 	        else {
-	        	console.log('there');
+	        	event.preventDefault();
 	        	var input = $('form').serializeArray();
 
-				// parse input data into a user JSON object
-				var user = {};
-				$.each(input, function(index, form_obj) {
-					if (form_obj.name == "expertise") {
-						var expertiseLevel = parseInt(form_obj.value)
-						if (expertiseLevel == 1){
-							user[form_obj.name] = "No Experience"
-						} else if (expertiseLevel <= 20){
-							user[form_obj.name] = "Beginner";
-						} else if (expertiseLevel <= 40) {
-							user[form_obj.name] = "Novice";
-						} else if (expertiseLevel <= 60) {
-							user[form_obj.name] = "Intermediate";
-						} else if (expertiseLevel <= 80) {
-							user[form_obj.name] = "Advanced";
-						} else if (expertiseLevel < 100) {
-							user[form_obj.name] = "Expert";
-						} else {
-							user[form_obj.name] = "Master";
-						}
-					}
-					else {
-						user[form_obj.name] = form_obj.value;
-					}
-				});
-				user['uid'] = firebase.auth().currentUser.uid;
-				console.log(user);
 
-				// get a new key for post
-				var newMentorKey = firebase.database().ref().child('mentors').push().key;
-				user['mentor_id'] = newMentorKey;
+	        	var profile = {};
+	        	profile['uid'] = firebase.auth().currentUser.uid;
+	        	profile['activities'] = {};
 
-				var updates = {};
-				updates['/mentors/' + newMentorKey] = user;
-				console.log(updates);
+	        	var profile_id = null;
+	        	firebase.database().ref('/users').child(firebase.auth().currentUser.uid).once('value').then(function(snapshot) {
+        			profile_id = snapshot.val().profile_id;
+	        	
+        			// go through the activity list and add it to the update object
+		        	var tmp_activity = null;
+		        	$.each(input, function(key, form_item) {
 
-				firebase.database().ref().update(updates);
+		        		// add the activity to the profile
+		        		if(form_item.name.toLowerCase().indexOf('activity') >= 0) {
+		        			if (form_item.value != null) {
+		        				tmp_activity = form_item.value;
+		        				profile.activities[tmp_activity] = 0;
+		        			}
+		        			else {
+		        				tmp_activity = null;
+		        			}
+		        		}
 
-				console.log(newMentorKey);
+		        		// add the expertise level of that activity to the profile
+		        		else if (form_item.name.toLowerCase().indexOf('expertise') >= 0) {
+		        			if (tmp_activity != null) {
+		        				if (form_item.value != null) {
+			        				profile.activities[tmp_activity] = form_item.value;
+			        			}
+			        			else {
+			        				delete profile.activities[tmp_activity];
+			        			}
+		        			}
+		        		}
+
+		        		// add any other relevant data to the profile
+		        		else {
+		        			profile[form_item.name] = form_item.value;
+		        		}
+		        	});
+		        	console.log(Object.keys(profile.activities).length);
+		        	if (profile.activities.hasOwnProperty("")) {
+		        		if (Object.keys(profile.activities).length == 1) {
+			        		delete(profile.activities);
+			        	}
+			        	else {
+			        		delete(profile.activities[""]);
+			        	}
+		        	}
+		        	console.log(profile)
+		        	firebase.database().ref('/profiles/' + profile_id).set(profile);
+	        	});
+
+	        	console.log(profile);
 
 				$('#exampleModal').modal('show');
 
@@ -74,20 +88,123 @@ $(document).ready(function() {
 	    });
 	}, false);
 
-
-
-
-	// make sure name field is properly generated
+	// code to make sure that the page is loaded correctly with user's profile info
 	firebase.auth().onAuthStateChanged(function(user) {
-	  if (user) {
-		var template = Handlebars.compile($("#form-name-value-template").html());
-		$(template(user)).insertAfter('#form-name-label');
-	  }
+
+		// compile the Handlebar templates
+		var name_template = Handlebars.compile($("#form-name-value-template").html());
+		var text_area_template = Handlebars.compile($("#form-bio-area-template").html());
+
+		// name data to be filled
+		$(name_template(user)).insertAfter('#form-name-label');
+
+		// retrieving the user's info here
+		firebase.database().ref('/users/' + user.uid).once('value').then(function(user_info) {
+
+
+			// search for the user's profile here
+			firebase.database().ref('/profiles').once('value').then(function(profiles) {
+
+				var prof_id = user_info.val().profile_id
+				var user_profile = profiles.val()[prof_id];
+
+				console.log(user_profile);
+
+				// bio data to be filled
+				$("#text-area-group").append(text_area_template(user_profile));
+
+				// if the profile exists, populate the page via this code
+				if (profiles.val()[prof_id].hasOwnProperty('activities')) {
+
+					if (user_profile.hasOwnProperty('activities')) {
+						// activity data to be filled
+						$.each(user_profile.activities, function(act, exp) {
+
+							// update the activity_input so that addRow contains the proper information
+							activity_input['activity_name'] = act;
+							activity_input['activity_expertise'] = exp;
+
+							addRow();
+
+							// selects the correct values
+							if (exp == 1) {
+								$('#adv-radio-' + (activity_input['activity_id']-1)).prop('checked', true);
+							}
+							else if (exp == 2) {
+								$('#exp-radio-' + (activity_input['activity_id']-1)).prop('checked', true);
+							}
+							else if (exp == 3) {
+								$('#mas-radio-' + (activity_input['activity_id']-1)).prop('checked', true);
+							}
+
+							// reset activity_input's act and exp
+							activity_input['activity_name'] = "";
+							activity_input['activity_expertise'] = "";
+						});
+					}
+				}
+				// otherwise default populate (add an empty activity template row)
+				else addRow();
+			});
+		});
 	});
 
 	$(".retHomeBtn").click(function() {
 		window.location = $(this).find("a").attr("href");
 		return false;
-	});
+    });
+    
+    // helper function to add a new row to the activity list
+    function addRow() {
+		var activity_template = Handlebars.compile($("#form-activity-row-template").html());
+
+    	// no more than 8 rows allowed
+        if (activity_input['counter'] >= 9){
+        	console.log("max number of rows added");
+            return;
+        }
+
+        // remove add buttons from all existing rows first
+        var row_btn = $(".form-row").find("button");
+        row_btn.off();
+        row_btn.removeClass("add-btn").addClass("remove-btn");
+		row_btn.find("i").removeClass("fa-plus").addClass("fa-times");
+
+		// appends a new row
+		$('#activity-list').append(activity_template(activity_input));
+
+		// if the row has reached maximum capacity, make sure last row has 'x'
+		if (activity_input['counter'] == 8) {
+        	var row_btn = $(".form-row").find("button");
+			row_btn.removeClass("add-btn").addClass("remove-btn");
+			row_btn.find("i").removeClass("fa-plus").addClass("fa-times");
+		}
+
+        activity_input['activity_id'] = activity_input['activity_id'] + 1;
+        activity_input['counter'] = activity_input['counter'] + 1;
+
+        $(".add-btn").on("click", addRow);
+        $(".remove-btn").on("click", deleteRow);
+
+        console.log(activity_input);
+    };
+
+    // helper function to delete rows and reset button functionality
+    function deleteRow() {
+        $(this).parent().parent().remove();
+        activity_input['counter'] = activity_input['counter'] - 1;
+
+        // make the last button always be plus (unless max capacity reached)
+        $(".form-row").find("button").off();
+
+        var row_btn = $(".form-row:last").find("button");
+        console.log(row_btn.name);
+        row_btn.removeClass("remove-btn").addClass("add-btn");
+		row_btn.find("i").removeClass("fa-times").addClass("fa-plus");
+        $(".add-btn").on("click", addRow);
+        $(".remove-btn").on("click", deleteRow);
+
+        console.log(activity_input);
+    };
 
 });
