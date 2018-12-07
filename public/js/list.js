cdfb649926218312a5f5099b6fedd7398c9190b1
Asianu <l6leung@ucsd.edu>
2018-12-07 01:18:03 -0800

    my list updated on mentor removal

--- public/js/list.js
+++ public/js/list.js
@@ -20,9 +20,6 @@ $(document).ready(function() {
 							if (mentor.val().hasOwnProperty("activities")) {
 								$("#mentor-table-body").append(mentor_template(mentor.val()));
 							}
-							$('.remove-btn').on('click', function(event) {
-								console.log(this);
-							});
 						});
 						$("#empty-mentor-row").hide();
 					});
@@ -46,8 +43,49 @@ $(document).ready(function() {
 				}
 			});
 		});
+	});
 
+	// removes the profile from the user's list of mentors
+	$(document).on('click', '.remove-btn', function(event) {
+		event.stopPropagation();
+		event.stopImmediatePropagation();
 
+		// uid refers to the mentor's uid
+		var uid = 0;
+
+		if ($(event.target).is('i')) {
+			uid = $(event.target).parent().attr('id');
+		}
+		else {
+			uid = event.target.id;
+		}
+
+		// remove the user from the other user's mentee list
+		firebase.database().ref('/users').child(firebase.auth().currentUser.uid).once('value').then(function(user) {
+			firebase.database().ref('/mentor_list').child(user.val().mentor_list).once('value').then(function(mentor_list) {
+
+				// match the key used to connect mentor/mentee pair and delete it
+				firebase.database().ref('/users').child(uid).once('value').then(function(other) {
+					var mentor_mentee_key = 0;
+
+					// searches and finds correct mentor/mentee key
+					$.each(mentor_list.val(), function(key, profile_id) {
+						if (profile_id == other.val().profile_id) {
+							mentor_mentee_key = key;
+						}
+					});
+
+					console.log(mentor_mentee_key);
+
+					firebase.database().ref('/mentor_list/' + user.val().mentor_list).child(mentor_mentee_key).remove();
+					firebase.database().ref('/mentee_list/' + other.val().mentee_list).child(mentor_mentee_key).remove();
+				});
+
+			});
+		});
+
+		// remove the other user from the user's mentor list
+		$("#row" + uid).remove();
 	});
 
 });
